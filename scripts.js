ec753016794967af5096c3a32e1e4b2bc9ff0067
eserb <81191959+eserb-cpu@users.noreply.github.com>
2023-04-03 11:14:11 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,4 @@
-// Replace with your ThingSpeak channel ID and read API key
-const CHANNEL_ID = '2092743';
-const READ_API_KEY = '2ZKCYCOZ0BH68HI4';
-
-async function getFillLevel() {
-  const url = `https://api.thingspeak.com/channels/${2092743}/fields/1/last?api_key=${2ZKCYCOZ0BH68HI4}`;
-  
-  try {
-    const response = await fetch(url);
-    
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-    
-    const fillLevel = await response.text();
-    return parseInt(fillLevel, 10);
-  } catch (error) {
-    console.error('Error fetching fill level:', error);
-    return null;
-  }
-}
-
-async function updateFillLevelDisplay() {
+function updateFillLevelDisplay() {
   const fillLevel = 42;
   const fillLevelRect = document.getElementById("fillLevelRect");
   const fillLevelPercentage = document.getElementById("fillLevelPercentage");
